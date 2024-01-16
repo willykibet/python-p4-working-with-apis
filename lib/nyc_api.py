9868b1284e5a728ca26956331f09c59332c9800a
Willy <willykipchumba22@gmail.com>
2024-01-16 21:59:28 +0300

    done

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 
@@ -10,5 +9,20 @@ class GetPrograms:
     response = requests.get(URL)
     return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_agencies(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list
+
+#programs = GetPrograms().get_programs()
+#print(programs)
+
+programs = GetPrograms()
+agencies = programs.program_agencies()
+
+for agency in set(agencies):
+    print(agency)
